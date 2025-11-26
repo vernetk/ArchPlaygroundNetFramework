a424d585fc19a76c796e4e32ac83d5d8882ebb36
vernetk <kevin1710@msn.com>
2025-11-26 22:43:09 +0100

    Ajoutez des fichiers projet.

---
+++ ArchPlaygroundNetFramework/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ArchPlaygroundNetFramework.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ArchPlaygroundNetFramework/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ArchPlaygroundNetFramework.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
